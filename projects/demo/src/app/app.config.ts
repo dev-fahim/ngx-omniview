edd4ea9857e5ddf0961737d2820da89c3dadd6cf
Khushi Joshi <khushi.joshi@binapani.com>
2025-10-30 13:11:50 +0530

    update: more improvements

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,11 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     importProvidersFrom(
-      HttpClientModule,
       MarkdownModule.forRoot()
     ),
   ]
